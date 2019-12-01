dcbb0197bc5f73ef39a0a4712f4cf8b728c86e2b
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-12-01 13:24:51 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2019
+
+Problems and Solutions for [Advent of Code](https://adventofcode.com/2019).
---
+++ src/days/day-1.test.ts
@@ -0,0 +1,10 @@
+describe('day 1', () => {
+  test.todo(
+    'For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.',
+  )
+  test.todo(
+    'For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.',
+  )
+  test.todo('For a mass of 1969, the fuel required is 654.')
+  test.todo('For a mass of 100756, the fuel required is 33583.')
+})
