8bb998398f541ea5c4a62a059c86db156eabedde
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-12-03 14:27:34 -0500

    setup part2 tests

--- src/days/day-3/day-3.test.ts
+++ src/days/day-3/day-3.test.ts
@@ -1,4 +1,5 @@
 import { distanceToClosestIntersection } from './part-1'
+import { fewestCombinedStepsFor } from './part-2'
 
 describe('day-3', () => {
   describe('part-1', () => {
@@ -23,4 +24,22 @@ describe('day-3', () => {
         ),
       ).toEqual(135))
   })
+
+  describe('part-2', () => {
+    test('input-1', () =>
+      expect(
+        fewestCombinedStepsFor(
+          ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
+          ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'],
+        ),
+      ).toEqual(610))
+  })
+
+  test('input-2', () =>
+    expect(
+      fewestCombinedStepsFor(
+        ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51'],
+        ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7'],
+      ),
+    ).toEqual(410))
 })
