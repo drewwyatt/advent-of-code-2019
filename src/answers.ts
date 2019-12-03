db29c00eee18f1efb69192ac50f4720660f42ab3
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-12-02 20:34:35 -0500

    automate import/export

--- src/answers.ts
+++ src/answers.ts
@@ -1,10 +1,15 @@
-import solutions from './days'
+// import solutions from './days'
+import getSolutions from './utils/get-solutions'
 
 const getAnswers = async () => {
+  const solutions = await getSolutions()
   const days = Object.keys(solutions)
-  const answers = await Promise.all(Object.values(solutions).map(fn => fn()))
-  answers.forEach((answer, idx) => {
-    console.log(days[idx], answer)
+  const answers = await Promise.all(
+    Object.values(solutions).map(fns => Promise.all(fns.map(f => f()))),
+  )
+  answers.forEach((answers, idx) => {
+    console.log(`📅 ${days[idx]}:`)
+    answers.forEach((a, i) => console.log(`  [Part ${i + 1}]: ${a}`))
   })
 }
 
---
+++ src/utils/get-solutions.ts
@@ -0,0 +1,37 @@
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+type Solution = () => Promise<number>
+type SolutionMap = Record<string, Solution[]>
+
+const DAYS_DIR = join(__dirname, '../days')
+
+const getDays = async (): Promise<SolutionMap> => {
+  const dirList = readdirSync(DAYS_DIR, { withFileTypes: true })
+    .filter(t => t.isDirectory())
+    .map(t => t.name)
+
+  const promises = dirList.map(async dir => {
+    let part1: Solution | undefined
+    let part2: Solution | undefined
+    try {
+      part1 = (await import(join(DAYS_DIR, dir, 'part-1'))).default
+    } catch {
+      /* */
+    }
+    try {
+      part2 = (await import(join(DAYS_DIR, dir, 'part-2'))).default
+    } catch {
+      /* */
+    }
+
+    return [part1, part2]
+  })
+
+  return (await Promise.all(promises)).reduce(
+    (acc, solutions, idx) => ({ ...acc, [dirList[idx]]: solutions.filter(Boolean) }),
+    {},
+  )
+}
+
+export default getDays
