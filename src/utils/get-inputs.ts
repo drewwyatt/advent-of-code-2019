66678ec7a2d7796271bd66fbe1ca75ad017be477
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-12-04 17:06:39 -0500

    remove noisy logs

--- src/utils/get-inputs.ts
+++ src/utils/get-inputs.ts
@@ -8,17 +8,13 @@ const PROMISE_MAP: Record<string, Promise<string>> = {}
 
 const getInputs = async <T>(inputFile: string, transform: (str: string) => T) => {
   if (!PROMISE_MAP[inputFile]) {
-    console.log('[Loading]:', inputFile)
     PROMISE_MAP[inputFile] = readFile(
       path.join(__dirname, '../../inputs', inputFile),
       'utf8',
     )
-  } else {
-    console.log('[Load Initialized]:', inputFile)
   }
 
   const file = await PROMISE_MAP[inputFile]
-  console.log('[Transforming]:', inputFile)
   return transform(file)
 }
 
